a566e55f6fb9d041c098d0442f1a7aad0012db84
nit-in <nit_in@live.com>
2020-08-23 01:58:25 +0530

    removing the accidental print statement

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 #!/bin/env python3
 from pydictionary import Dictionary
 
-print("Hello")
-
 if __name__ == "__main__":
     while True:
-        term = input("Enter the word to search:\t")
+        term = input("\nEnter the word to search:\t")
         dict = Dictionary(str(term))
         dict.meaning()
         dict.synonyms()
