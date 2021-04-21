0aab84827ba8096430a334487080cf4a74985025
nit-in <nit-in@users.noreply.github.com>
2021-04-21 11:57:39 +0530

    testing workflow

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Py-Dictionary",
-    version="3.1.0",
+    version="3.1.1",
     author="nit-in",
     author_email="nit_in@live.com",
     description="Dictionary module",
