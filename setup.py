2868afd68a2635bef9ad207bde8596a3d5de7db8
nit-in <nit_in@live.com>
2021-09-02 21:02:32 +0530

    bump up version
    Signed-off-by: nit-in <nit_in@live.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Py-Dictionary",
-    version="4.1.2",
+    version="4.1.4",
     author="nit-in",
     author_email="nit_in@live.com",
     description="Dictionary module",
